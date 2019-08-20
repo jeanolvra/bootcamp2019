f9183573e595763d3d0b2c1ce68313b504897672
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-19 21:20:49 -0300

    Query & Routers Paraams

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,8 +4,13 @@ const server = express();
 
 // localhost:3000/teste
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hellow World" });
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = { "name": "Jean", "email": "seujeanoliveira@gmail.com"  }
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `Buscando o usuario ${id}` });
 });
 
 server.listen(3000);
