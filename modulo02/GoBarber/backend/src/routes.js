8e3cc8d5768a7a6a14f6be77147ae3df2b7313b7
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-22 20:21:05 -0300

    Criando loader de models

--- modulo02/GoBarber/backend/src/routes.js
+++ modulo02/GoBarber/backend/src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jean Oliveira',
+    email: 'jean@joyjet.com',
+    password_hash: '32541010',
+  });
+  return res.json(user);
+});
 
 export default routes;
