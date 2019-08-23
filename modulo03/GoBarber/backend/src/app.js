c670390eacecef58755260e2a1299652bbe7dd24
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-23 14:16:54 -0300

    Iniciando modulo03

---
+++ modulo03/GoBarber/backend/src/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import routes from './routes';
+import './database';
+
+// Criando a classe App
+class App {
+  constructor() {
+    // This se refera á classe
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+// Instanciondo o Objeto App
+export default new App().server;
---
+++ modulo03/README.md
@@ -0,0 +1,16 @@
+# Modulo 02 - Iniciando o back-end do GoBarber
+
+Neste modulo foram abordados os seguintes assuntos:
+
+1. Nodemon & Sucrase to ES6
+2. Conceitos do Docker
+3. Configuração do Docker
+4. Sequelize, Arquitetura MVC, Migrations, Seeds
+5. ESLint, Prettier & EditorConfig
+6. Migrations
+7. Models
+8. Gerando hash de passwords
+9. Autenticação JWT
+10. Middlewares de autenticação
+11. Validação com Yup
+12. Postgres
