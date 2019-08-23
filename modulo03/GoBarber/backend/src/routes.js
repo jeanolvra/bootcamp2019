373d9fc36634654ff515ecfe3556c4f0d4dc626a
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-23 15:36:50 -0300

    Configurando upload de arquivos com Muilter

--- modulo03/GoBarber/backend/src/routes.js
+++ modulo03/GoBarber/backend/src/routes.js
@@ -1,16 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import fileController from './app/controllers/fileController';
 
 import authMiddeware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddeware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), fileController.store);
 
 export default routes;
