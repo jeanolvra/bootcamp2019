a47882d9a62ff1f3d9a9ade1be359364b9c08a2d
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-23 16:00:32 -0300

    Listando providers

--- modulo03/GoBarber/backend/src/routes.js
+++ modulo03/GoBarber/backend/src/routes.js
@@ -4,7 +4,8 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
-import fileController from './app/controllers/fileController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddeware from './app/middlewares/auth';
 
@@ -15,8 +16,11 @@ routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddeware);
+
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), fileController.store);
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
