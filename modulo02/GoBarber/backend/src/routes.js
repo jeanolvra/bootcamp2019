2987597a294ca86ddd29004737e36b4fd25a45fc
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-22 21:13:09 -0300

    Criando usuarior

--- modulo02/GoBarber/backend/src/routes.js
+++ modulo02/GoBarber/backend/src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Jean Oliveira',
-    email: 'jean@joyjet.com',
-    password_hash: '32541010',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
