5aa913661b8de7bc96567ddd102073b91d094289
Jean Oliveira <seujeanoliveira@gmail.com>
2019-08-26 19:08:53 -0300

    [MODULO 03] Mailer & AppointmentController Mail

---
+++ modulo03/GoBarber/backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5d774b92e83792',
+    pass: '8558f642ab065b',
+  },
+  default: {
+    from: 'Jean Oliveira <noreply@gobarber.com>',
+  },
+};
